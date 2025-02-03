b1669c84df24ac91d0b718a36d0e5e336d7ed2ed
ignasiialemany <ignasialemanyjuv@gmail.com>
2025-02-03 19:35:35 +0000

    Modified flask website app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import nbconvert
 import nbformat
 import os
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -22,10 +23,17 @@ def convert_notebook_to_html(ipynb_path):
 def index():
     return render_template('index.html')
 
+@app.route('/bayesian_linear_regressions')
+def project_bayesian_linear_regressions():
+    with open('BayesianLinearRegression.html', 'r') as file:
+        notebook_html = file.read()
+    soup = BeautifulSoup(notebook_html, 'html.parser')
+    body_content = soup.body.encode_contents().decode()
+    return render_template('BayesianLinearRegression.html',notebook_content=notebook_html,title="Bayesian Linear Regressions")
+
 @app.route('/project_PINNs')
 def project_PINNs():
-    notebook_html = convert_notebook_to_html('PINNs_notebook.ipynb')
-    return render_template('projects_PINNs.html',notebook_content=notebook_html)
+    return render_template('PCA.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
